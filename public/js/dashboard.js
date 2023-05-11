53525d16d13d9e2a523a3aa3cd6c9791275bb5ca
Ian Danas <ianmdanas@gmail.com>
2023-05-10 19:07:43 -0700

    adding delete functionality

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -22,3 +22,40 @@ addPostbtn.addEventListener("click",e=>{
         }
     })
 })
+
+deletePostbtn = document.querySelector('#delPost')
+
+deletePostbtn.addEventListener("click",e=>{
+    e.preventDefault();
+    fetch(`/api/post/${document.querySelector('#post').getAttribute('data-id')}`,{
+        method:"DELETE",
+        headers:{
+            "Content-Type":"application/json"
+        }
+    }).then(res=>{
+        if(res.ok){
+           location.href = "/"
+        } else {
+            alert("trumpet sound")
+        }
+    })
+})
+
+deletePostbtn = document.querySelector('#delPost')
+
+deletePostbtn.addEventListener("click",e=>{
+    e.preventDefault();
+    fetch(`/api/post/${document.querySelector('#post').getAttribute('data-id')}`,{
+        method:"DELETE",
+        headers:{
+            "Content-Type":"application/json"
+        }
+    }).then(res=>{
+        if(res.ok){
+           location.href = "/"
+        } else {
+            alert("trumpet sound")
+        }
+    })
+})
+
--- public/js/home.js
+++ public/js/home.js
@@ -1,22 +1,23 @@
-const addCommentbtn = document.querySelectorAll('#addComment')
+const addCommentbtn = document.querySelectorAll('.card')
 
-for (i of addCommentbtn) {
-    i.addEventListener("click",async (e)=>{
-        e.preventDefault();
-       postID = i.getAttribute('data-postID')
-       const userObj = {
-        content:document.querySelector("#content").value,
-        post_id:postID
-    }
-    console.log(userObj)
-      const comment = await fetch("/api/comment",{
-           method:"POST",
-           body:JSON.stringify(userObj),
-           headers:{
-               "Content-Type":"application/json"
-           }
-       })
-       location.reload()
-   })
+// for (let i = 0; i < addCommentbtn.length; i++) {
+//     const element = addCommentbtn[i];
+//     element.addEventListener("click",async (e)=>{
+//        postID = element.getAttribute('data-postID')
+//        const userObj = {
+//         content:document.querySelector("#content").value,
+//         post_id:postID
+//     }
+//     console.log(userObj)
+//       const comment = await fetch("/api/comment",{
+//            method:"POST",
+//            body:JSON.stringify(userObj),
+//            headers:{
+//                "Content-Type":"application/json"
+//            }
+//        })
+//        location.reload()
+//        console.log(userObj)
+//    })
    
-}
+// }
